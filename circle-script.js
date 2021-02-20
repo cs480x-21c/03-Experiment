10e962b8dabe68170252e5f0ee688141aba5d517
Isabel Alvarado Blanco Uribe <alvaradoblancouribe@gmail.com>
2021-02-19 23:14:45 -0500

    adding circle bar graph code

---
+++ circle-script.js
@@ -0,0 +1,51 @@
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 10, bottom: 10, left: 10},
+        width = 460 - margin.left - margin.right,
+        height = 800 - margin.top - margin.bottom,
+        innerRadius = 100,
+        outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
+    
+    // append the svg object to the body of the page
+    var svg = d3.select("#circle-bar")
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + ( height/2+100 )+ ")"); // Add 100 on Y translation, cause upper bars are longer
+  
+    function loadGraph(){
+      var xArray=[]
+      var numberOfGraphs = 5
+      for(var i=0; i<=numberOfGraphs;i++){
+        xArray[i]={i:""+i+"", height: Math.round(Math.random()*10000+10) }
+      }  
+      console.log(xArray)
+        // X scale
+      var x = d3.scaleBand()
+          .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
+          .align(0)                  // This does nothing ?
+          .domain(xArray.map(function(d){return(d.i)})); // The domain of the X axis is the list of states.
+    
+        // Y scale
+      var y = d3.scaleRadial()
+          .range([innerRadius, outerRadius])   // Domain will be define later.
+          .domain([0, 10000]); // Domain of Y is from 0 to the max seen in the data
+    
+      // Add bars
+      svg.append("g")
+        .selectAll("path")
+        .data(xArray)
+        .enter()
+        .append("path")
+          .attr("fill", "#69b3a2")
+          .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+              .innerRadius(innerRadius)
+              .outerRadius(function(d) {return (y((d.height)))})
+              .startAngle(function(d) { return(x(d.i)); })
+              .endAngle(function(d) { return( x(d.i)+ x.bandwidth()) })
+              .padAngle(0.01)
+              .padRadius(innerRadius))
+    }
+    loadGraph()
+   
+
