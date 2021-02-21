a03b9649ddd063de152cf384341a9282e9a06cca
Suryanshg <Suryanshg47@gmail.com>
2021-02-21 16:39:56 -0500

    Centred circle-chart

--- visualizations.js
+++ visualizations.js
@@ -184,8 +184,8 @@ function generateBarChart() {
 function generateCircleChart() {
 
     var margin = {top: 10, right: 10, bottom: 10, left: 10},
-        width = 460 - margin.left - margin.right,
-        height = 800 - margin.top - margin.bottom,
+        width = 500 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom,
         innerRadius = 100,
         outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
     
@@ -194,7 +194,7 @@ function generateCircleChart() {
         .style("width", width + margin.left + margin.right)
         .style("height", height + margin.top + margin.bottom)
       .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + ( height/2+100 )+ ")"); // Add 100 on Y translation, cause upper bars are longer
+        .attr("transform", "translate(" + width / 2 + "," + ( height/2 )+ ")"); // Add 100 on Y translation, cause upper bars are longer
 
     var xArray=[]
     var numberOfGraphs = 5
