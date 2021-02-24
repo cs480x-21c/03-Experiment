5e9534eeef149ee5788f1dbf4ba13cb88bc31f7f
bgmsadoques <bgmsadoques@wpi.edu>
2021-02-24 17:55:59 -0500

    Moved deployed code over, fixed the server and client sides so new files are created with the next possible result number

--- scripts/Results/ResultsController.js
+++ scripts/Results/ResultsController.js
@@ -4,13 +4,46 @@ class ResultsController
 {
     makeNewResult()
     {
-        this.resultIndex = 0; // needs to be changed for the new file
+        // Default
+        this.resultIndex = 0;
 
-        this.result = {children:[]};
+        this.result = {children:[], fileName:"result" + this.resultIndex + ".json"};
         this.entry = {trialIndex: 0, chartType: "",
             correctAnswer: 0, participantAnswer:0};
 
         this.trialIndex = 0;
+
+        try
+        {
+            // Gets the next possible result index, used to name the result file
+            let options =
+                {
+                    method: 'POST',
+                    headers: {"Content-type": "application/json; charset=UTF-8"},
+                };
+
+            const promise = fetch('/resultIndex', options);
+            promise.then(response =>
+            {
+                if(!response.ok)
+                {
+                    console.error(response)
+                }
+                else
+                {
+                    return response.json();
+                }
+            }).then(result =>
+            {
+                // Changes the file name to match the new result
+                this.resultIndex = result;
+                this.result.fileName = "result" + this.resultIndex + ".json";
+            })
+        }
+        catch (e)
+        {
+            console.log(e);
+        }
     }
 
 
@@ -31,16 +64,36 @@ class ResultsController
     saveResult()
     {
         // We cannot save files on the server file system from pure js
-        //
+        // Sends the information to the Node.js running on the server
 
+        try {
+            let options =
+                {
+                    method: 'POST',
+                    headers: { "Content-type": "application/json; charset=UTF-8"},
+                    body: JSON.stringify(this.result)
+                }
 
-
+            const promise = fetch('/result', options);
+            promise.then(response =>
+            {
+                if(!response.ok)
+                {
+                    console.error(response)
+                }
+                else
+                {
+                    return response.json();
+                }
+            }).then(result =>
+            {
+                console.log(result);
+            })
+        }
+        catch (e)
+        {
+            console.log(e);
+        }
     }
-
-
-
-
-
-
 
 }
--- server.js
+++ server.js
@@ -4,7 +4,10 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+var bodyParser = require("body-parser");
+let fs = require("fs");
 const app = express();
+var jsonParser = bodyParser.json();
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
@@ -20,3 +23,69 @@ app.use(express.static("scripts"));
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+//
+
+app.post('/result', jsonParser, (request, response) => {
+  let result = request.body;
+  let fileName = result.fileName;
+
+  delete result.fileName;
+
+  console.log(result);
+
+
+
+
+  fs.writeFile("results/"+fileName, JSON.stringify(result), function (err){
+    if (err) {
+      return console.log(err);
+    }
+    else
+    {
+      console.log(fileName + " was saved");
+    }
+
+  });
+
+  response.json("SUCC");
+})
+
+app.post('/resultIndex', async (request, response) => {
+
+  let resultIndex = 0;
+
+  fs.readdir("results/", (err, files) => {
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach((file) =>
+    {
+      // Do whatever you want to do with the file
+      try
+      {
+        let currentResultIndex = parseInt(file.substring(6).replace(".json", ""), 10);
+        console.log(currentResultIndex);
+        if (currentResultIndex > resultIndex)
+        {
+
+          resultIndex = currentResultIndex;
+        }
+      }
+      catch
+      {
+        // not a proper results file, ignore it
+      }
+
+    });
+
+    // Add 1 to the result index
+    response.json(resultIndex + 1);
+  });
+
+
+})
+
