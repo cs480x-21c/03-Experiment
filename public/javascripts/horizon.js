85d4aaf197de88269239f772820f8cbc32a9780f
masun77 <masun@wpi.edu>
2021-02-23 18:57:25 -0800

    hcharts

--- public/javascripts/horizon.js
+++ public/javascripts/horizon.js
@@ -67,15 +67,15 @@ function makeChart(svgID, width, height, xRange, yRange, fileName, chartNum) {
                 .enter().append('div')
                 .attr('class', 'horizon')
                 .each(function (d, i) {
-                    if (i == 0) {
+                    if (i === 0) {
                         horizonChart.title('Dataset A')
                             .call(this, d);
-                    } else if (i == colsMarksNumSetsX[1][1] - colsMarksNumSetsX[1][0]) {
+                    } else if (i === colsMarksNumSetsX[1][1] - colsMarksNumSetsX[1][0]) {
                         horizonChart.title('Dataset B')
                             .call(this, d);
                     }
                     else {
-                        horizonChart.call(this, d);
+                        horizonChart.title('').call(this, d);
                     }
                 });
 
