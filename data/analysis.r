2f2f8ae7c8f004de3743a7a1a2b13df776420bbf
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-02-25 17:55:00 -0500

    added t tests for confidence intervals

--- data/analysis.r
+++ data/analysis.r
@@ -5,11 +5,20 @@
 
 library(ggplot2)
 library(Hmisc)
+library(tidyverse)
 
 pdf(NULL)
 
 data <- read.csv('./data/data_final.csv')
-ggplot(data, aes(x = Type, y = cm.error)) + stat_summary(fun.data = 'mean_cl_boot', colour = 'red', size = 0.5)
-#stat_summary(mapping = NULL, data = NULL, geom = "pointrange", position = "identity")
 
+ggplot(data, aes(x = Type, y = cm.error)) + stat_summary(fun.data = 'mean_cl_boot', colour = 'red', size = 0.5)
 ggsave('./img/plot.png')
+
+
+piecharts <- filter(data, Type == 'Piechart')
+boxplots <- filter(data, Type == 'Boxplot')
+barcharts <- filter(data, Type == 'Barchart')
+
+t.test(piecharts$cm.error, ci = 0.95)
+t.test(boxplots$cm.error, ci = 0.95)
+t.test(barcharts$cm.error, ci = 0.95)
