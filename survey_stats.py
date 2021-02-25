eb5b1791b424c9042fc880b392243cca7a2e8def
Clay OL <clay.oshiroleavitt@gmail.com>
2021-02-25 14:40:25 -0500

    added check for exact match to survey_stats

--- survey_stats.py
+++ survey_stats.py
@@ -10,5 +10,7 @@ records['difference'] = records.difference.abs()
 
 records['cm_error'] = np.log2(records['difference'] + 0.125)
 
+records.loc[(records.cm_error == -3.0), 'cm_error' ] = 0.0
+
 print(records.head())
 records.to_csv(os.path.join("survey_records.csv"))
