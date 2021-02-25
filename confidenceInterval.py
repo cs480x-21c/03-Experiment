500a15ffe8565fee282782b298db15845ac2493e
Suryanshg <Suryanshg47@gmail.com>
2021-02-25 16:57:53 -0500

    Added code for CI as well as image of the results

--- confidenceInterval.py
+++ confidenceInterval.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("trials.csv")
 # arr = np.random.uniform(low=0,high=100,size=100)
-arr = [1,2,3,4,5]
-print(list(arr))
+errorBar = np.array(df.loc[df.vis == 'bar']['error'])
+errorCircle = np.array(df.loc[df.vis == 'circle']['error'])
+errorPie = np.array(df.loc[df.vis == 'pie']['error'])
 
 def createBootStraps(data,size):
     bootStraps = np.empty(size)
@@ -12,6 +15,20 @@ def createBootStraps(data,size):
         bootStraps[i] = np.mean(sample)
     return bootStraps
 
-result = createBootStraps(arr,1000)
-# print(result)
-print(np.mean(result))
+bootstrappedErrorBar = createBootStraps(errorBar,100)
+bootstrappedErrorCircle = createBootStraps(errorCircle,100)
+bootstrappedErrorPie = createBootStraps(errorPie,100)
+
+intervals = [np.percentile(bootstrappedErrorBar,[2.5,97.5]),
+             np.percentile(bootstrappedErrorPie,[2.5,97.5]),                                              
+             np.percentile(bootstrappedErrorCircle,[2.5,97.5])] 
+means = [np.mean(bootstrappedErrorBar),np.mean(bootstrappedErrorPie),np.mean(bootstrappedErrorCircle)]
+vis = ['bar','pie','circle']
+
+for i in range(len(vis)):
+    plt.plot(tuple(intervals[i]),(i+1,i+1),'r|-') # Plotting the 95% Bootstrap Confidence Interval
+    plt.plot(means[i],i+1,marker='o',color='black') # Plotting the log2 scaled error
+plt.yticks(range(1,len(vis)+1),vis)
+plt.xlabel("Log2 Error")
+plt.ylabel("Visualizations")
+plt.show()
