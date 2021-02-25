e980773bb615d563423eaae7c17acb731a007823
N'yoma Diamond <34525496+nyoma-diamond@users.noreply.github.com>
2021-02-25 18:11:36 -0500

    changed t intervals for bootstrapped intervals

--- data/analysis.r
+++ data/analysis.r
@@ -2,10 +2,11 @@
 #install.packages("ggrepel", repos = "http://cran.us.r-project.org") # if not already installed
 #install.packages("tidyverse", repos = "http://cran.us.r-project.org") # if not already installed
 #install.packages("Hmisc", repos = "http://cran.us.r-project.org") # if not already installed
+#install.packages("boot", repos = "http://cran.us.r-project.org") # if not already installed
 
-library(ggplot2)
 library(Hmisc)
 library(tidyverse)
+library(boot)
 
 pdf(NULL)
 
@@ -19,6 +20,6 @@ piecharts <- filter(data, Type == 'Piechart')
 boxplots <- filter(data, Type == 'Boxplot')
 barcharts <- filter(data, Type == 'Barchart')
 
-t.test(piecharts$cm.error, ci = 0.95)
-t.test(boxplots$cm.error, ci = 0.95)
-t.test(barcharts$cm.error, ci = 0.95)
+smean.cl.boot(piecharts$cm.error, conf.int = .95, B = 1000)
+smean.cl.boot(boxplots$cm.error, conf.int = .95, B = 1000)
+smean.cl.boot(barcharts$cm.error, conf.int = .95, B = 1000)
