035837e52cc67269d6df606a8264028f4e0f5b37
Clay OL <clay.oshiroleavitt@gmail.com>
2021-02-25 15:46:21 -0500

    added mean calculation to R script

--- bootstrapCI.R
+++ bootstrapCI.R
@@ -4,5 +4,9 @@ library(Hmisc)
 
 records = read.csv('survey_records.csv', header=TRUE)
 
+aggregate( x = records$cm_error, by = list(records$graphs_guessed), FUN=mean)
+
+
 ggplot( records, aes(x = cm_error, y = graphs_guessed)) +
   stat_summary(fun.data = "mean_cl_boot")
+
