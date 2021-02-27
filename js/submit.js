d07190e586856bb1fc8aa8284d6fa69aeef74a2f
Jason Dykstra <jasondykstra2000@gmail.com>
2021-02-26 19:32:44 -0500

    allowed enter key to submit answer

--- js/submit.js
+++ js/submit.js
@@ -6,16 +6,23 @@ function textBoxSubmit() {
     document.body.appendChild(div);
 
     // Text box
-    var textInput = document.createElement("INPUT");
+    var textInput = document.createElement("input");
     textInput.setAttribute("placeholder", "Enter Percent E.g. 50");
     textInput.setAttribute("type", "text");
     div.appendChild(textInput);
+
+    // Put a % icon next to the text box
     var percentIcon = document.createElement("Percent");
     var percentIconText = document.createTextNode("%");
     percentIcon.id = "percentIcon";
     percentIcon.style.fontFamily = "arial";
     percentIcon.appendChild(percentIconText);
     div.appendChild(percentIcon);
+
+    // Allow text box to submit when enter key is pressed
+    textInput.addEventListener("keydown", function(e){
+        if(e.keyCode == 13) checkSubmit();
+    });
 
     // Submit button
     var submit = document.createElement("Button");
@@ -25,7 +32,11 @@ function textBoxSubmit() {
     // Function for when a request is submitted
     submit.appendChild(submitText);
     div.appendChild(submit);
-    submit.onclick = function () {
+    submit.onclick = function(){
+        checkSubmit();
+    };
+
+    function checkSubmit(){
         if (textInput.value <= 100 && textInput.value > 0 && textInput.value != '') {
             numSubmits++;
             var text = textInput.value;
@@ -46,6 +57,5 @@ function textBoxSubmit() {
         } else {
             alert("Please type a number between 0 and 100 (Inclusive for 100)");
         }
-
     }
 }
